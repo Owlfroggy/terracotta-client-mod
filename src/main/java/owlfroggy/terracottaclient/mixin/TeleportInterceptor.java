7e53ded683a186ee1f0d54a513c80ceaae988472
Owlfroggy <140653655+Owlfroggy@users.noreply.github.com>
2026-07-21 18:18:00 -0400

    fix TeleportReceiver's onTeleport oldPos argument not actually being the pre-teleport position

--- src/main/java/owlfroggy/terracottaclient/mixin/TeleportInterceptor.java
+++ src/main/java/owlfroggy/terracottaclient/mixin/TeleportInterceptor.java
@@ -2,7 +2,9 @@ package owlfroggy.terracottaclient.mixin;
 
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,10 +12,17 @@ import owlfroggy.terracottaclient.TCClient;
 
 @Mixin(ClientPacketListener.class)
 public class TeleportInterceptor {
+    @Unique
+    Vec3 oldPos;
+
+    @Inject(method = "handleMovePlayer", at = @At("HEAD"))
+    private void posGrabber(ClientboundPlayerPositionPacket packet, CallbackInfo info) {
+        if (TCClient.MCI.player != null) oldPos = TCClient.MCI.player.position();
+    }
 	@Inject(method = "handleMovePlayer", at = @At("RETURN"))
 	private void init(ClientboundPlayerPositionPacket packet, CallbackInfo info) {
         if (TCClient.MCI.player != null) {
-            TCClient.fireTeleportReceivers(packet.change().position(),TCClient.MCI.player.position());
+            TCClient.fireTeleportReceivers(packet.change().position(),oldPos);
         }
 	}
 }
